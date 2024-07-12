6a5cb55f6dec99899a56228c913a3ff456534328
HassanAliRizvi <hasanijthedi2002@gmail.com>
2024-07-12 12:23:08 -0400

    port added

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -16,7 +16,7 @@ const corsOption = {
 connectDB();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Middleware setup
 app.use(express.urlencoded({ extended: true }));
